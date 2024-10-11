f2eb3eb36f33f1f236efd70457490d71b4095f16
Fuzi1324 <114992886+Fuzi1324@users.noreply.github.com>
2024-10-11 14:27:34 +0200

    added pwa functionality

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,4 +4,65 @@
     <h1>PlantPal</h1>
 
     <h2>Willkommen, USERNAME!</h2>
+
+    <p id="install-text">PlantPal-App installieren:</p>
+    <button id="install-button" style="display: none;">Install App</button>
+
+    <script>
+        let deferredPrompt;
+        const installButton = document.getElementById('install-button');
+        const installText = document.getElementById('install-text');
+
+        function isInStandaloneMode() {
+            return (window.matchMedia('(display-mode: standalone)').matches) || (window.navigator.standalone === true);
+        }
+
+        function updateUI() {
+            if (isInStandaloneMode()) {
+                installText.style.display = 'none';
+                installButton.style.display = 'none';
+            } else {
+                if (window.localStorage.getItem('plantpal_installed') === 'true') {
+                    installText.textContent =
+                        'Die PlantPal-App ist bereits installiert. Öffnen Sie diese direkt auf Ihrem Gerät!';
+                    installButton.style.display = 'none';
+                } else {
+                    installText.textContent = 'PlantPal-App installieren:';
+                    installButton.textContent = 'Install App';
+                    installButton.style.display = 'none';
+                }
+            }
+        }
+
+        function handleInstallButtonClick() {
+            if (deferredPrompt) {
+                deferredPrompt.prompt();
+                deferredPrompt.userChoice.then((choiceResult) => {
+                    if (choiceResult.outcome === 'accepted') {
+                        console.log('User accepted the install prompt');
+                    } else {
+                        console.log('User dismissed the install prompt');
+                    }
+                    deferredPrompt = null;
+                });
+            }
+        }
+
+        installButton.addEventListener('click', handleInstallButtonClick);
+
+        window.addEventListener('beforeinstallprompt', (event) => {
+            event.preventDefault();
+            deferredPrompt = event;
+            installButton.style.display = 'block';
+            installText.textContent = 'PlantPal-App installieren:';
+            installButton.textContent = 'Install App';
+        });
+
+        window.addEventListener('appinstalled', () => {
+            console.log('PWA wurde installiert');
+            window.localStorage.setItem('plantpal_installed', 'true');
+            location.reload();
+        });
+        updateUI();
+    </script>
 @endsection
--- resources/views/layout/mainLayout.blade.php
+++ resources/views/layout/mainLayout.blade.php
@@ -4,8 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name', 'LawogWeb') }}</title>
-    <link rel="icon" href="{{ url('images/favicon.ico') }}">
+    <title>{{ config('app.name', 'PlantPal') }}</title>
+    <link rel="icon" href="{{ url('./images/logo.ico') }}">
+
+    <meta name="theme-color" content="#6777ef" />
+    <link rel="apple-touch-icon" href="{{ asset('./images/logo.png') }}">
+    <link rel="manifest" href="{{ asset('/manifest.json') }}">
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
         crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
@@ -48,6 +53,20 @@
     </div>
 
     @livewireScripts
+
+    <script src="{{ asset('/sw.js') }}"></script>
+
+    <script>
+        if (!navigator.serviceWorker.controller) {
+
+            navigator.serviceWorker.register("/sw.js").
+
+            then(function(reg) {
+                console.log("Service worker has been registered for scope: " + reg.scope);
+            });
+        }
+    </script>
+
 </body>
 
 </html>
