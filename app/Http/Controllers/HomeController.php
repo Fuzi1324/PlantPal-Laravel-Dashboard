bc7d6978211e64e50f7873f99ac16b98286f625d
Fuzi1324 <114992886+Fuzi1324@users.noreply.github.com>
2024-10-21 19:30:02 +0200

    started implementing mqtt-connection

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Console\Command;
+use PhpMqtt\Client\Facades\MQTT;
+
+class HomeController extends Controller
+{
+
+    protected $signature = 'mqtt:subscribe';
+
+    protected $description = 'Subscribe to MQTT topic';
+
+    public function handle()
+    {
+        $mqtt = MQTT::connection();
+        $mqtt->subscribe('devices/+/status', function (string $topic, string $message) {
+            echo sprintf('Received message on topic [%s]: %s', $topic, $message);
+        });
+
+        $mqtt->loop(true);
+        return Command::SUCCESS;
+        //NNSXS.LFYMTWHYN3246XQS3YNWJGS6VDNUQVLLJKK43NY.FEPPMDLVOBEARFCMRAO77VQAIFJXOME6SGSDIDWW2EOLUE4O2TQA
+    }
+
+
+    public function index()
+    {
+        return view('home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+/*Route::get('/', function () {
     return view('home');
 });
+*/
